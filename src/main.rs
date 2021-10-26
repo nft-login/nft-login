0deab993b69430e4434113878fdf3ac83d6e63c6
Christian M <chriamue@gmail.com>
2021-10-26 21:04:08 +0200

    :sparkles: well known configuration endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,68 @@
 #[macro_use]
 extern crate rocket;
+use rocket::State;
 
-use openidconnect::core::{CoreJsonWebKeySet, CoreRsaPrivateSigningKey};
-use openidconnect::{JsonWebKeyId, PrivateSigningKey};
+use openidconnect::core::{
+    CoreClaimName, CoreJsonWebKeySet, CoreJwsSigningAlgorithm, CoreProviderMetadata,
+    CoreResponseType, CoreRsaPrivateSigningKey, CoreSubjectIdentifierType,
+};
+use openidconnect::{
+    AuthUrl, EmptyAdditionalProviderMetadata, IssuerUrl, JsonWebKeyId, JsonWebKeySetUrl,
+    PrivateSigningKey, ResponseTypes, Scope, TokenUrl,
+};
+
+struct Config {
+    pub ext_hostname: String,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/.well-known/openid-configuration")]
+fn configuration(config: &State<Config>) -> Result<String, std::io::Error> {
+    let provider_metadata = CoreProviderMetadata::new(
+        IssuerUrl::new(config.ext_hostname.to_string()).unwrap(),
+        AuthUrl::new(format!("{}/authorize", config.ext_hostname).to_string()).unwrap(),
+        JsonWebKeySetUrl::new(
+            format!("{}/jwk", config.ext_hostname)
+                .to_string()
+                .to_string(),
+        )
+        .unwrap(),
+        vec![
+            ResponseTypes::new(vec![CoreResponseType::Code]),
+            ResponseTypes::new(vec![CoreResponseType::Token, CoreResponseType::IdToken]),
+        ],
+        vec![CoreSubjectIdentifierType::Pairwise],
+        vec![CoreJwsSigningAlgorithm::RsaSsaPssSha256],
+        EmptyAdditionalProviderMetadata {},
+    )
+    .set_token_endpoint(Some(
+        TokenUrl::new(format!("{}/token", config.ext_hostname).to_string()).unwrap(),
+    ))
+    .set_scopes_supported(Some(vec![
+        Scope::new("openid".to_string()),
+        Scope::new("email".to_string()),
+        Scope::new("profile".to_string()),
+    ]))
+    .set_claims_supported(Some(vec![
+        CoreClaimName::new("sub".to_string()),
+        CoreClaimName::new("aud".to_string()),
+        CoreClaimName::new("email".to_string()),
+        CoreClaimName::new("email_verified".to_string()),
+        CoreClaimName::new("exp".to_string()),
+        CoreClaimName::new("iat".to_string()),
+        CoreClaimName::new("iss".to_string()),
+        CoreClaimName::new("name".to_string()),
+        CoreClaimName::new("given_name".to_string()),
+        CoreClaimName::new("family_name".to_string()),
+        CoreClaimName::new("picture".to_string()),
+        CoreClaimName::new("locale".to_string()),
+    ]));
+
+    serde_json::to_string(&provider_metadata).map_err(From::from)
 }
 
 #[get("/jwk")]
@@ -24,5 +80,11 @@ fn jwk() -> String {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, jwk])
+    let config = Config {
+        ext_hostname: "https://localhost:8000".to_string(),
+    };
+
+    rocket::build()
+        .mount("/", routes![index, configuration, jwk])
+        .manage(config)
 }
