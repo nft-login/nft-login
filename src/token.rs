8b90b54e9b4e3f7f1ada1f43beafef3639289979
Christian M <chriamue@gmail.com>
2021-11-01 19:44:16 +0100

    :sparkles::art: return extra claims

--- src/token.rs
+++ src/token.rs
@@ -1,13 +1,13 @@
 use chrono::{Duration, Utc};
 use openidconnect::core::{
-    CoreIdTokenFields, CoreJwsSigningAlgorithm, CoreRsaPrivateSigningKey, CoreTokenResponse,
-    CoreTokenType,
+    CoreGenderClaim, CoreJsonWebKeyType, CoreJweContentEncryptionAlgorithm,
+    CoreJwsSigningAlgorithm, CoreRsaPrivateSigningKey, CoreTokenType,
 };
 use openidconnect::{
-    AccessToken, Audience, EmptyAdditionalClaims, EmptyExtraTokenFields, EndUserUsername, IdToken,
-    IdTokenClaims, IssuerUrl, JsonWebKeyId, StandardClaims, SubjectIdentifier,
+    AccessToken, AdditionalClaims, Audience, EmptyExtraTokenFields, EndUserUsername, IdToken,
+    IdTokenClaims, IdTokenFields, IssuerUrl, JsonWebKeyId, StandardClaims, StandardTokenResponse,
+    SubjectIdentifier, TokenResponse,
 };
-use openidconnect::{AdditionalClaims, TokenResponse};
 use rocket::form::Form;
 use rocket::State;
 use serde::{Deserialize, Serialize};
@@ -40,10 +40,7 @@ pub async fn token_endpoint(
 }
 
 #[post("/token", data = "<post_data>")]
-pub async fn post_token_endpoint(
-    tokens: &State<Tokens>,
-    post_data: Form<PostData>,
-) -> String {
+pub async fn post_token_endpoint(tokens: &State<Tokens>, post_data: Form<PostData>) -> String {
     let mutex = tokens.muted.lock().unwrap();
     let token = mutex.get(&post_data.code);
     match token {
@@ -53,13 +50,24 @@ pub async fn post_token_endpoint(
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Claims {
-    account: String,
-    nonce: String,
-    signature: String,
+pub struct Claims {
+    pub account: String,
+    pub nonce: String,
+    pub signature: String,
 }
 
 impl AdditionalClaims for Claims {}
+
+pub type NftIdTokenFields = IdTokenFields<
+    Claims,
+    EmptyExtraTokenFields,
+    CoreGenderClaim,
+    CoreJweContentEncryptionAlgorithm,
+    CoreJwsSigningAlgorithm,
+    CoreJsonWebKeyType,
+>;
+
+pub type NftTokenResponse = StandardTokenResponse<NftIdTokenFields, CoreTokenType>;
 
 pub async fn token(
     config: &Config,
@@ -67,10 +75,10 @@ pub async fn token(
     nonce: Option<String>,
     account: Option<String>,
     signature: Option<String>,
-) -> CoreTokenResponse {
+) -> NftTokenResponse {
     let rsa_pem = config.rsa_pem.clone();
 
-    let _claims = Claims {
+    let claims = Claims {
         account: account.clone().unwrap_or_default(),
         nonce: nonce.clone().unwrap_or_default(),
         signature: signature.clone().unwrap_or_default(),
@@ -80,31 +88,10 @@ pub async fn token(
         IdTokenClaims::new(
             IssuerUrl::new(config.ext_hostname.clone()).unwrap(),
             vec![Audience::new(client_id)],
-            // The ID token expiration is usually much shorter than that of the access or refresh
-            // tokens issued to clients.
             Utc::now() + Duration::seconds(300),
-            // The issue time is usually the current time.
             Utc::now(),
-            // Set the standard claims defined by the OpenID Connect Core spec.
-            StandardClaims::new(
-                // Stable subject identifiers are recommended in place of e-mail addresses or other
-                // potentially unstable identifiers. This is the only required claim.
-                SubjectIdentifier::new(account.clone().unwrap_or_default()),
-            )
-            .set_preferred_username(Some(
-                EndUserUsername::new(format!(
-                    "{};{}{}",
-                    signature.unwrap_or_default(),
-                    account.unwrap_or_default(),
-                    nonce.clone().unwrap_or_default()
-                ))
-                .into(),
-            )),
-            // OpenID Connect Providers may supply custom claims by providing a struct that
-            // implements the AdditionalClaims trait. This requires manually using the
-            // generic IdTokenClaims struct rather than the CoreIdTokenClaims type alias,
-            // however.
-            EmptyAdditionalClaims {},
+            StandardClaims::new(SubjectIdentifier::new(account.clone().unwrap_or_default())),
+            claims,
         ),
         // The private key used for signing the ID token. For confidential clients (those able
         // to maintain a client secret), a CoreHmacKey can also be used, in conjunction
@@ -130,9 +117,9 @@ pub async fn token(
     )
     .unwrap();
 
-    CoreTokenResponse::new(
+    NftTokenResponse::new(
         AccessToken::new(nonce.unwrap_or_default()),
         CoreTokenType::Bearer,
-        CoreIdTokenFields::new(Some(id_token), EmptyExtraTokenFields {}),
+        NftIdTokenFields::new(Some(id_token), EmptyExtraTokenFields {}),
     )
 }
